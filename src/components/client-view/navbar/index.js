9cfd6f974d3708167443aeee70deb11a664c3a52
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-12-10 03:30:14 +0100

    navbar

--- src/components/client-view/navbar/index.js
+++ src/components/client-view/navbar/index.js
@@ -1,5 +1,32 @@
 "use client"
-
+const menuItems = [
+    {
+        id : 'home',
+        label : 'Home'
+    },
+    {
+        id:"about",
+        label:"About"
+    },
+    {
+        id:"experience",
+        label:"Experience"
+    },
+    {
+        id:"project",
+        label:"Project"
+    },
+    {
+        id:"contact",
+        label:"Contact"
+    },
+    
+]
 export default function Navbar(){
-    return<div>Navbar</div>
+    return<>
+    <header>
+        <nav className="max-w-screen-xl px6 sm:px-8 lg:px-16 mx-auto grid grid-flow-col" ></nav>
+    </header>
+    
+    </>
 }
