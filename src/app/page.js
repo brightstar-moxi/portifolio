10bc5c681ae2ce9e29920724379e2eb351866fa2
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-12-10 02:45:21 +0100

    page layout updated

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,22 @@ import ClientHomeView from '@/components/client-view/home'
 import ClientProjectView from '@/components/client-view/project'
 
 
-export default function Home() {
+async function extractAllDatas(currentSection){
+  const res = await fetch(`http://localhost:3000/api/${currentSection}/get`, {
+    method:"GET",
+    cache: 'no-store'
+  })
+  const data = await res.json()
+
+  return data;
+}
+
+export default async function Home() {
+  const homeSectionData = await extractAllDatas('home')
+  const aboutSectionData = await extractAllDatas('about')
+  const experienceSectionData = await extractAllDatas('experience')
+  const projectSectionData = await extractAllDatas('project')
+  const contactSectionData = await extractAllDatas('contact')
   return (
     <div>
       <ClientHomeView />
