e5cd1dfc24a68f77f163849a70b92fbd6aedfb2a
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-12-12 04:55:29 +0100

    home update

--- src/components/client-view/home/index.js
+++ src/components/client-view/home/index.js
@@ -1,8 +1,27 @@
 "use client"
+import { motion } from "framer-motion"
 
-export default function ClientHomeView({data}){
+import AnimationWapper from "../animation-wrapper";
 
-    return(
-        <div>Home</div>
+function variants(){
+return{
+    offscreen :{
+        y:150,
+        opacity : 0
+    },
+    onscreen
+}
+}
+
+export default function ClientHomeView({ data }) {
+
+    return (
+        <div className="max-w-screen-xl mt-24 px-8 xl:px-16 mx-auto" id="home">
+            <AnimationWapper className={
+                "grid grid-flow-row sm:grid-flow-col grid-rows-2 md:grid-rows-1 sm:grid-cols-2 gap-8 py-6 sm:py-16"
+            }>
+                <motion.div variants={setVariants}></motion.div>
+            </AnimationWapper>
+        </div>
     )
 }
--- src/components/client-view/navbar/index.js
+++ src/components/client-view/navbar/index.js
@@ -1,6 +1,8 @@
 "use client"
 
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import { Link as LinkScroll, scroller } from "react-scroll";
+
 
 const menuItems = [
     {
@@ -24,7 +26,26 @@ const menuItems = [
         label: "Contact"
     },
 
-]
+];
+
+function CreateMenus({ activeLink, getMenuItems, setActiveLink }) {
+    return getMenuItems.map((item) => <LinkScroll
+        activeClass="active"
+        to={item.id}
+        spy={true}
+        smooth={true}
+        duration={1000}
+        onSetActive={() => setActiveLink(item.id)}
+        className={`px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative
+ ${activeLink === item.id
+                ? "text-green-main animation-active shadow-green-main"
+                : "text-[#000] font-bold hover:text-green-main"
+            }`}
+    >
+        {item.label}
+    </LinkScroll>)
+}
+
 export default function Navbar() {
     const [activeLink, setActiveLink] = useState('home');
     const [scrollActive, SetScrollActive] = useState(false);
@@ -35,7 +56,7 @@ export default function Navbar() {
         })
     }, [])
     return <>
-        <header className={`fixed top-0 wfull z-30 bg-white-500 transition-all ${scrollActive ? "shadow-md pt-0" : "pt-4"}`}>
+        <header className={`fixed top-0 w-full z-30 bg-white-500 transition-all ${scrollActive ? "shadow-md pt-0" : "pt-4"}`}>
             <nav className="max-w-screen-xl px6 sm:px-8 lg:px-16 mx-auto grid grid-flow-col py-3 sm:py-4" >
                 <div className="col-start-1 col-end-2 flex items-center">
                     <div className=" cursor-pointer flex gap-2 font-bold items-center text-[20px] text-green-main">
@@ -45,8 +66,29 @@ export default function Navbar() {
                         ortfolio
                     </div>
                 </div>
+                <ul className="hidden lg:flex col-start-4 col-end-8 text-[#000] items-center">
+                    <CreateMenus
+                        setActiveLink={setActiveLink}
+                        activeLink={activeLink}
+                        getMenuItems={menuItems}
+                    />
+                </ul>
+                <div className="col-start-10 col-end-12 font-medium flex justify-center items-center">
+                    <button className="py-3 px-6 border-[2px] bg-[#fff] border-green-main text-[#000] font-semibold rounded-lg text-xl tracking-widest hover:shadow-green-md  transition-all outline-none">Contact Me</button>
+                </div>
             </nav>
         </header>
+        <nav className="fixed lg:hidden bottom-0 left-0 right-0 z-20 px-4 sm:px-8 shadow-t">
+            <div className="bg-white-500 sm:px-3">
+                <ul className="overflow-x-auto flex w-full justify-between items-center text-[#000]">
+                    <CreateMenus
+                        setActiveLink={setActiveLink}
+                        activeLink={activeLink}
+                        getMenuItems={menuItems}
+                    />
+                              </ul>
+            </div>
+        </nav>
 
     </>
 }
