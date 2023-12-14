2254dfaf99c8f532a67e8d91d4cfd8f9af6b426b
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-12-14 03:33:20 +0100

    home update

--- src/components/client-view/home/index.js
+++ src/components/client-view/home/index.js
@@ -3,6 +3,7 @@ import { motion } from "framer-motion"
 
 import AnimationWapper from "../animation-wrapper";
 import { useMemo } from "react";
+import { FaFacebookF, FaLinkedinIn, FaInstagram, FaTwitter } from 'react-icons/fa'
 
 function variants() {
     return {
@@ -33,15 +34,29 @@ export default function ClientHomeView({ data }) {
                     }
                     variants={setVariants}>
                     <div className="flex flex-col justify-center items-start row-start-2 sm:row-start-1">
-                        <h1 className="mb-4 text-3xl lg:text">
+                        <h1 className="mb-4 text-3xl lg:text-4xl xl:text-6xl font-medium leading-normal">
                             {
-                                data?.heading.spilt(" ").map((item, index) => <span
-                                    className={`${index === 2 || index === 3 ? "text-green-main" : "text-[#000]"}`}
-                                >
-                                    {item}
-                                </span>)
+                                data && data.length ? data[0]?.heading
+                                    .split(" ").map((item, index) => (<span
+                                        className={`${index === 2 || index === 3 ? "text-green-main" : "text-[#000]"}`}
+                                    >
+                                        {item} {" "}
+                                    </span>))
+                                    : null
                             }
                         </h1>
+                        <p className="text[#000] mt-4 mb-8 font-bold">
+                            {
+                                data && data.length ? data[0]?.summary : null
+                            }
+                            <motion.div className="flex gap-3">
+                                <FaFacebookF color="rgba(13, 183, 96, 1)" className="w-[40px] h-[40px] " />
+                                <FaTwitter color="rgba(13, 183, 96, 1)" className="w-[40px] h-[40px] " />
+                                <FaLinkedinIn color="rgba(13, 183, 96, 1)" className="w-[40px] h-[40px] " />
+                                <FaInstagram color="rgba(13, 183, 96, 1)" className="w-[40px] h-[40px] " />
+
+                            </motion.div>
+                        </p>
                     </div>
                 </motion.div>
             </AnimationWapper>
