eac30a5ff91430e29d7bee686f19af45789e0763
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-12-15 03:57:39 +0100

    about update

--- src/components/client-view/about/index.js
+++ src/components/client-view/about/index.js
@@ -21,50 +21,65 @@ function variants() {
     }
 }
 
-export default function ClientAboutView({data}){
-console.log(data, 'about');
+export default function ClientAboutView({ data }) {
+    console.log(data, 'about');
 
     const setVariants = useMemo(() => variants(), []);
 
     const aboutDataInfo = [
         {
-            label : 'Client',
-            value : data?.noofclients || '0'
+            label: 'Client',
+            value: data?.noofclients || '0'
         },
         {
-            label : 'Project',
-            value : data?.noofprojects || '0'
+            label: 'Projects',
+            value: data?.noofprojects || '0'
         },
         {
-            label : 'Experience',
-            value : data?.yearofexperience|| '0'
+            label: 'Experience',
+            value: data?.yearofexperience || '0'
         },
-       
-    ]
-    return(
+
+    ];
+
+    const headingText = "Why Should You Hire Me For Your Next Project?"
+    return (
         <div className="max-w-screen-xl mt-24 mb-6 sm:mt-14 sm:mb-14 px-6 sm:px-8 lg:px-16 mx-auto" id="about">
-            <div>
+            <div className="w-full flex">
                 <AnimationWapper className="rounded-lg w-full grid-flow-row grid grid-cols-1 sm:grid-cols-3 py-9 divide-y-2 sm:divide-y-0 sm:divide-x-2 divide-green-main bg-ehite-500 z-10">
                     {
-                        aboutDataInfo.map((infoItem, index)=>
-                        <motion.div className={`flex items-center justify-start ${index === 0 ? "sm:justify-start" : index === 1 ? 'sm:justify-center' : 'sm:justify-end'}
+                        aboutDataInfo.map((infoItem, index) => (
+                            <motion.div className={`flex items-center justify-start ${index === 0 ? "sm:justify-start" : index === 1 ? 'sm:justify-center' : 'sm:justify-end'}
                         py-4 sm:py-6 w-8/12 px-4 sm:w-auto mx-auto sm:mx-0
                         `}
-                        key={index}
-                        custom={{duration : 2 + index}}
-                        variants={setVariants}
-                        >
-                            <div className="flex m-0 w-40 sm:w-auto">
-                                <div className="flex flex-col">
-                                    <p className="text-[50px] text-green-main">{infoItem.value}</p>
-                                    <p className="text-[25px] font-bold text-[#000000]">{infoItem.label}</p>
+                                key={index}
+                                custom={{ duration: 2 + index }}
+                                variants={setVariants}
+                            >
+                                <div className="flex m-0 w-40 sm:w-auto">
+                                    <div className="flex flex-col">
+                                        <p className="text-[50px] text-green-main">{infoItem.value}</p>
+                                        <p className="text-[25px] font-bold text-[#000000]">{infoItem.label}</p>
+                                    </div>
                                 </div>
-                            </div>
-                        </motion.div>
-                        )
+                            </motion.div>
+                        ))
                     }
                 </AnimationWapper>
             </div>
+            <AnimationWapper className={'pt-6'}>
+                <div className="flex flex-col justify-center items-center row-start-2 sm:row-start-1">
+                    <h1 className="leading-[70px] mb-4 text-3xl lg:text-4xl xl:text-5xl font-medium">
+                        {
+                            headingText.split(' ').map((item, index) => (
+                                <span className={`${index === 8 ? 'text-green-main' : 'text-[#000]'}`}
+                                >{item} {" "}</span>
+                            ))
+                        }
+                    </h1>
+                    <p className="text-[#000] mt-4 mb-8 font-bold">{data?.aboutme}</p>
+                </div>
+            </AnimationWapper>
         </div>
     )
 }
