7189f84b47798fa40708dd42ddd88f22afc5bd45
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-12-17 02:46:27 +0100

    project page updated

--- src/components/client-view/project/index.js
+++ src/components/client-view/project/index.js
@@ -1,9 +1,37 @@
 "use client"
 
+import { useMemo, useRef } from "react";
 import AnimationWapper from "../animation-wrapper";
+import { motion, useScroll } from "framer-motion";
+
+function variants() {
+    return {
+        offscreen: {
+            y: 150,
+            opacity: 0
+        },
+        onscreen: ({ duration = 2 } = {}) => ({
+            y: 0,
+            opacity: 1,
+            transition: {
+                type: 'spring',
+                duration
+            }
+        })
+    }
+}
 
 export default function ClientProjectView({data}){
 console.log(data, "Client Project View");
+
+
+
+const setVariants = useMemo(() => variants(), []);
+const containerRef = useRef(null);
+const {scrollXProgress} = useScroll
+({container: containerRef})
+
+
     return(
         <div className="max-w-screen-xl mt-24 mb-6 sm:mt-14 sm:mb-14 px-6 sm:px-8 lg:px-16 mx-auto" id="project">
             <AnimationWapper className={'py-6 sm:py-16'}>
@@ -11,13 +39,64 @@ console.log(data, "Client Project View");
                     <h1 className="leading-[70px] mb-4 text-3xl lg:text-4xl xl:text-5xl font-medium">
                         {
                             "My Projects".split(' ').map((item, index) => (
-                                <span className={`${index === 8 ? 'text-green-main' : 'text-[#000]'}`}
+                                <span className={`${index === 1 || index === 3 ? 'text-green-main' : 'text-[#000]'}`}
                                 >{item} {" "}</span>
                             ))
                         }
                     </h1>
+                    <svg id="progress" width={100} height={100} viewBox="0 0 100 100">
+            <circle
+              cx={"50"}
+              cy={"50"}
+              r="30"
+              pathLength={"1"}
+              className="stroke-[#000]"
+            />
+            <motion.circle
+              cx={"50"}
+              cy={"50"}
+              r="30"
+              pathLength={"1"}
+              className="stroke-green-main"
+              style={{ pathLength: scrollXProgress }}
+            />
+          </svg>
                   
                 </div>
+            </AnimationWapper>
+            <AnimationWapper>
+            <ul className="project-wrapper" ref={containerRef}>
+                {
+                    data && data.length ? data.map((item,index)=>(
+                        <li className="w-full flex items-stretch cursor-pointer"
+                        key={index}>
+                             <div className="border-2 w-full relative border-green-main transition-all rounded-lg flex flex-col">
+                             <div className="flex p-4 flex-col xl:flex-row w-full items-stretch xl:items-center">
+                             <div className="flex order-2 xl:order-1">
+                                <div className="flex flex-col">
+                                    <h3 className="text-3xl text-black-600 capitalize font-extrabold">{item.name}</h3>
+                                    <p className="text-sm mt-2 text-black-500 capitalize font-bold">
+                            {item.createdAt.split("T")[0]}
+                            <div className="grid gap-2 mt-5 grid-cols-2 h-full max-h-[200px] w-full">
+                                {
+                                    item?.technologies.split(",").map((techItem)=>(
+                                        <div className="w-full flex justify-start items-center">
+                                              <button className="whitespace-nowrap text-ellipsis overflow-hidden py-3 w-[120px] px-6 border-[2px] border-green-main bg-[#fff] text-[#000] font-semibold rounded-lg text-xs tracking-widest hover:shadow-green-main transition-all outline-none">
+                                        {techItem}
+                                    </button>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                          </p>
+                                </div>
+                             </div>
+                             </div>
+                             </div>
+                        </li>
+                    )) : null
+                }
+            </ul>
             </AnimationWapper>
         </div>
     )
