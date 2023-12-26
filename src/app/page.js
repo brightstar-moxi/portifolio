5cc9d9e695698cc4341df25820a4b98d0910216a
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-12-26 06:27:28 +0100

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -5,32 +5,41 @@ import ClientexprienceView from '@/components/client-view/experience'
 import ClientHomeView from '@/components/client-view/home'
 import ClientProjectView from '@/components/client-view/project'
 
+async function extractAllDatas(currentSection) {
+  try {
+    const res = await fetch(`http://localhost:3000/api/${currentSection}/get`, {
+      method: "GET",
+      cache: 'no-store'
+    });
 
-async function extractAllDatas(currentSection){
-  const res = await fetch(`http://localhost:3000/api/${currentSection}/get`, {
-    method:"GET",
-    cache: 'no-store'
-  })
-  const data = await res.json()
+    if (!res.ok) {
+      throw new Error(`HTTP error! Status: ${res.status}`);
+    }
 
-  return data && data.data;
+    const data = await res.json();
+    return data && data.data;
+  } catch (error) {
+    console.error("Error fetching data:", error.message);
+    return null; // Handle error gracefully, return null or an empty array/object
+  }
 }
 
 export default async function Home() {
-  const homeSectionData = await extractAllDatas('home')
-  const aboutSectionData = await extractAllDatas('about')
-  const experienceSectionData = await extractAllDatas('experience')
-  const educationSectionData = await extractAllDatas('education')
-  const projectSectionData = await extractAllDatas('project')
-  // const contactSectionData = await extractAllDatas('contact')
+  const homeSectionData = await extractAllDatas('home');
+  const aboutSectionData = await extractAllDatas('about');
+  const experienceSectionData = await extractAllDatas('experience');
+  const educationSectionData = await extractAllDatas('education');
+  const projectSectionData = await extractAllDatas('project');
+  // const contactSectionData = await extractAllDatas('contact');
+
   return (
     <div>
-      <ClientHomeView data= {homeSectionData} />
-      <ClientAboutView data={aboutSectionData && aboutSectionData.length ? aboutSectionData[0] : []}/>
-      <ClientexprienceView educationData={educationSectionData} experienceData={experienceSectionData}/>
+      <ClientHomeView data={homeSectionData} />
+      <ClientAboutView data={aboutSectionData && aboutSectionData.length ? aboutSectionData[0] : null} />
+      <ClientexprienceView educationData={educationSectionData} experienceData={experienceSectionData} />
       {/* <ClientEducationView/> */}
       <ClientProjectView data={projectSectionData} />
-      <ClientContactView  />
+      <ClientContactView />
     </div>
-  )
+  );
 }
