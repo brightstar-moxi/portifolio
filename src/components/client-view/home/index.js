a7241a0f1b589e60c26673db12ba1dafc6ff395e
brightstar-moxi <brightstarmoxiz2@gmail.com>
2026-09-05 03:35:22 +0100

    updated commit 9

--- src/components/client-view/home/index.js
+++ src/components/client-view/home/index.js
@@ -142,7 +142,7 @@ import Link from "next/link";
 
 import { Typewriter } from "react-simple-typewriter";
 import AnimationWrapper from "../animation-wrapper";
-import allImage from "../../../assets/my.png";
+import allImage from "../../../assets/pii.png";
 import { Link as LinkScroll } from "react-scroll";
 import { FiChevronDown } from "react-icons/fi";
 
